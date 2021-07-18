103da9a629baab60417274f80db58424cd190060
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-07-17 21:19:23 -0600

    packer: auto-install if it isn't installed

--- lua/plugins/_packer.lua
+++ lua/plugins/_packer.lua
@@ -1,6 +1,15 @@
-vim.cmd [[packadd packer.nvim]]
--- git clone https://github.com/wbthomason/packer.nvim\
---  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+        'git', 'clone', 'https://github.com/wbthomason/packer.nvim',
+        install_path
+    })
+    execute 'packadd packer.nvim'
+end
+
 return require('packer').startup(function(use)
     -- To handle Lua configuration
     use {'wbthomason/packer.nvim', 'tami5/sql.nvim'}
