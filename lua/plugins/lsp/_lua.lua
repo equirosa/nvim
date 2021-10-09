6aae82ccde383522edcbe6e6110709e5f478ea31
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-10-09 05:21:39 -0600

    add capabilities to the server configs for completion

--- lua/plugins/lsp/_lua.lua
+++ lua/plugins/lsp/_lua.lua
@@ -1,8 +1,11 @@
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 require'lspconfig'.sumneko_lua.setup {
+    capabilities = capabilities,
     cmd = {'lua-language-server'},
     settings = {
         Lua = {
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,13 +1,16 @@
 require'lspinstall'.setup() -- important
 
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
 local servers = require'lspinstall'.installed_servers()
 for _, server in pairs(servers) do
-    if server == "java" then require'lspconfig'[server].setup {} end
+    if server == "java" then
+        require'lspconfig'[server].setup {capabilities = capabilities}
+    end
 end
 
 require('plugins.lsp._lspkind')
 require('plugins.lsp._lua')
-
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+require'lspconfig'.rnix.setup {capabilities = capabilities}
