58693bdd25d23c17149903084c30bfec033dd8bf
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-10-11 16:59:27 -0600

    set up luasnip for nvim_cmp

--- lua/plugins/_nvim-cmp.lua
+++ lua/plugins/_nvim-cmp.lua
@@ -9,5 +9,11 @@ cmp.setup({
         ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'}),
         ['<CR>'] = cmp.mapping.confirm({select = true})
     },
-    sources = {{name = 'path'}, {name = 'buffer'}, {name = 'nvim_lsp'}}
+    snippet = {
+        expand = function(args) require'luasnip'.lsp_expand(args.body) end
+    },
+    sources = {
+        {name = 'path'}, {name = 'buffer'}, {name = 'nvim_lsp'},
+        {name = 'luasnip'}
+    }
 })
